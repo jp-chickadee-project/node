3ca3b4eaafca4dbd6982fcd5ff214794e02b955c
jonathon lefler <lefler.t.jonathon@gmail.com>
2019-11-15 20:30:14 +0000

    changed apt to apt-get

--- nodeSetup.sh
+++ nodeSetup.sh
@@ -6,15 +6,15 @@ echo "Adding user pi to dialout"
 usermod -a -G dialout pi
 cd /home/pi
 echo "Downloading git"
-apt install -y git
+apt-get install -y git
 echo "Enabling push down button"
 echo 'dtoverlay=gpio-shutdown, gpio_pin=3' >> /boot/config.txt
 echo "Installing rpi.gpio"
-apt install -y rpi.gpio
+apt-get install -y rpi.gpio
 echo "installing numpy"
-apt install -y python3-numpy
+apt-get install -y python3-numpy
 echo "Installing pip3"
-apt install -y python3-pip
+apt-get install -y python3-pip
 echo "Installing pyserial"
 #pip3 install -y pyserial
 apt-get install -y python3-serial
@@ -26,7 +26,7 @@ echo 'dtparam=spi=on' >> /boot/config.txt
 echo "Downloading LMIC"
 git clone https://github.com/jp-chickadee-project/lmic-rpi-lora-gps-hat.git
 chown -R pi:pi lmic-rpi-lora-gps-hat
-cd /home/pi/lmic-rpi-lora-gps-hat.git
+cd /home/pi/lmic-rpi-lora-gps-hat
 git checkout $name
 cd /home/pi
 echo "Downloading Mastercode"
@@ -38,14 +38,7 @@ chown -R pi:pi MasterCode
 echo "Adding brain.py to /etc/rc.local"
 sed -i '$i \python3 /home/pi/MasterCode/brain.py &\n' /etc/rc.local
 echo "Downloading WiringPi"
-#git clone git://git.drogon.net/wiringPi
-#chown -R pi:pi wiringPi
-#echo "Installing WiringPi"
-#cd /home/pi/wiringPi
-#git pull origin
-#./build
-#cd /home/pi
-apt install -y wiringpi
+apt-get install -y wiringpi
 echo "Enabling SSH."
 systemctl enable ssh
 echo "Changing keyboard layout"
@@ -54,8 +47,8 @@ echo "Changing Hostname"
 sed -i -e "s/raspberrypi/$name/g" /etc/hostname
 sed -i -e "s/raspberrypi/$name/g" /etc/hosts
 echo "Installing fail2ban"
-apt install -y fail2ban
-apt install -y figlet
+apt-get install -y fail2ban
+apt-get install -y figlet
 cd /home/pi/node
 ./login_setup.sh
 mv brain.service /lib/systemd/system
